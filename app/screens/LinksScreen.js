dca600732b6ed11dfa1bdc72e1c1bb38eb7ab6ad
dikobraz <ilnar.khusaenov@gmail.com>
2019-05-13 20:26:53 +0300

    0.4.5

--- app/screens/LinksScreen.js
+++ app/screens/LinksScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {AsyncStorage, Animated, Button,FlatList, ScrollView, StyleSheet,Picker, Text, View } from 'react-native';
 import {Avatar, Icon, ListItem, CheckBox } from 'react-native-elements';
-import MyListItem from '../components/ListItem'
+import { Table, TableWrapper, Row } from 'react-native-table-component';
 
 export default class LinksScreen extends React.Component {
   constructor(props){
@@ -9,11 +9,16 @@ export default class LinksScreen extends React.Component {
     this.state = {
       list:[],
       items:[],
-      group:0,
+      group:5433,
       groupList:[],
+      lessons:[],
+      tableHead: ['#', 'Студент',],
+      widthArr: [30, 160, ],
     }
-    this._getGroupList('2331');
+    this._getGroupList('5433');
     this._getGroupsList();
+    this._getLessonsList();
+    
   }
   static navigationOptions = {
     title: 'Журнал',
@@ -47,9 +52,9 @@ export default class LinksScreen extends React.Component {
     let id = await AsyncStorage.getItem('userId');
     let url = await AsyncStorage.getItem('url') + '/edu/groups';
     await fetch(url + '?id='+id)
-    .then(res=>res.json())
-    .then(res=>this.setState({list:res}))
-    .catch(err=>console.warn('err'))
+      .then(res=>res.json())
+      .then(res=>this.setState({list:res}))
+      .catch(err=>console.warn('err'))
 
     days = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
     list = []
@@ -67,20 +72,43 @@ export default class LinksScreen extends React.Component {
     );
     this.setState({items:pickerItemsList})
   };
-   /**
-   * Инициация каждого элемента списка
-   */
-  _renderItem = ({item, i}) => (
-    <MyListItem
-      key={i}
-      item={item}
-      id={this.state.groupList.indexOf(item)}
-      onPressItem={this._onPressItem}
-      onCheckItem={this._onCheckItem}
-      setItemMark={this._setItemMark}
-    />
-  );
+  _getLessonsList = async () => {
+    let id = await AsyncStorage.getItem('userId');
+    let url = await AsyncStorage.getItem('url') + '/lessons';
+    await fetch(url + '?userId='+id)
+      .then(res=>res.json())
+      .then(res=>this.setState({lessons:res}))
+  }
   render() {
+    const state = this.state;
+    const tableData = [];
+    if(state.lessons.length != 0) {
+      var subjs = state.list.map(val=>{
+        if(val.gNum == state.group){
+          return val.id
+        }
+      })
+
+      for (let i = 0; i < state.groupList.length; i += 1) {
+        const rowData = [];
+        for (let j = 0; j < 2 + state.lessons.length; j += 1) {
+          if(j==0) rowData.push(i+1<10?'0'+(i+1):i+1)
+          if(j==1) rowData.push(state.groupList[i].name + ' ' + state.groupList[i].surname)
+          if(j>1) {
+            if(subjs.indexOf(state.lessons[j-2].subjId) != -1){
+              if(state.lessons[j-2].content[i].mark) rowData.push(state.lessons[j-2].content[i].mark)
+              else if(state.lessons[j-2].content[i].checked) rowData.push("+")
+              else rowData.push('-')
+            } 
+          }
+          if(i==0  && state.lessons[j-2]) {
+            state.tableHead.push(state.lessons[j-2].date)
+            state.widthArr.push(50)
+          }
+        }
+        tableData.push(rowData);
+      }
+    }
     return (
       <View style={styles.container}>
         <View style={{height:50, flexDirection:"row", backgroundColor:'#3F51B5', borderBottomWidth:1,}}>
@@ -92,19 +120,40 @@ export default class LinksScreen extends React.Component {
             onValueChange={(itemValue, itemIndex) => {
               this.setState({group:itemValue})
               this._getGroupList(itemValue)
+              this.setState({
+                list:[],
+                items:[],
+                lessons:[],
+                tableHead: ['#', 'Студент',],
+                widthArr: [30, 160, ],
+              })
             }}>
             {this.state.items}
           </Picker>
           <View style={{flex:0.3}}></View>
         </View>
-        <View style={{flex:1}}>
-          { <FlatList
-              data={this.state.groupList}
-              extraData={this.state}
-              keyExtractor={index => String(index)}
-              renderItem={this._renderItem}
-          />}
-        </View>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderColor: '#C1C0B9'}}>
+              <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.text}/>
+            </Table>
+            <ScrollView style={styles.dataWrapper}>
+              <Table borderStyle={{borderColor: '#C1C0B9'}}>
+                {
+                  tableData.map((rowData, index) => (
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      textStyle={styles.text}
+                    />
+                  ))
+                }
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
       </View>
     );
   }
@@ -116,4 +165,8 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     backgroundColor:'#C5CAE9',
   },
+  header: { height: 50, backgroundColor: '#537791' },
+  text: { textAlign: 'center', fontWeight: '100' },
+  dataWrapper: { marginTop: -1 },
+  row: { height: 40, backgroundColor: '#E7E6E1' }
 });
